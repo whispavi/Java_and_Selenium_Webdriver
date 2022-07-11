680717533659bd3a5c66684c9b6042a9396ac016
Tori Vorobei <83900419+whispavi@users.noreply.github.com>
2022-07-11 15:03:54 +0300

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,13 +1,13 @@
 public class Data {
 
-    public String getUrl = "https://saas-userprofile.identite.us/#/";
+    public String getUrl = "https://url/#/";
     public String firstName = "Nika";
     public String lastName = "White";
-    public String email = "uskuq";
-    public String login = "uskuq";
-    public String password = "n123M";
-    public String getUrlPS = "https://ps-saas-clw.identite.us/";
-    public String getUrlGit = "https://github.com/whispavi/";
+    public String email = "***";
+    public String login = "***";
+    public String password = "***";
+    public String getUrlPS = "https://url/";
+  
 
 
 
